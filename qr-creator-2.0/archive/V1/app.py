5c35ed4e5c874cf14bbef12e03cdda7771c0b2e7
ath0rus <65640442+ath0rus@users.noreply.github.com>
2022-05-03 11:19:04 +1000

    Delete qr-creator-2.0 directory

--- qr-creator-2.0/archive/V1/app.py
+++
@@ -1,9 +0,0 @@
-import qrcode as qr
-
-try:
-    img = qr.make(str(input("Mesage to turn to qr code: ")))
-    print("Qr code saved")
-except:
-    print("Qr code creation error failed, Please try again")
-    
-img.save("qr2.png")
